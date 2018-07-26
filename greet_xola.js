077f340d2b519a8711a6d89f333af9e7026733f2
Londiwe <celelondiwe3@gmail.com>
2018-07-26 15:29:43 +0200

    installed chalk

--- greet_xola.js
+++ greet_xola.js
@@ -1,4 +1,22 @@
 
 
+/*const greet = require('./greet');
+console.log(greet('Londy'));*/
+
+/*const chalk = require('chalk');
+//import the greet module that is in the current folder
 const greet = require('./greet');
-console.log(greet('Londy'));
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)*/
+
+var figlet = require('figlet');
+
+figlet('Hello Londy!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
