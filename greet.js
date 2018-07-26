0ed8d87acd52ece40e1e1482707e54308e22552a
Londiwe <celelondiwe3@gmail.com>
2018-07-26 10:31:45 +0200

    greet

---
+++ greet.js
@@ -0,0 +1,13 @@
+/*var greet = function(name){
+var greeting = 'Hello, ' + name;
+  return greeting;
+  //console.log(greeting);
+};
+var msg = greet('Londy');
+console.log(msg);*/
+
+
+
+module.exports = function(name) {
+  return 'Hello, ' + name;
+}
